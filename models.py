a8e001ef7beaa1d5300ecf2b50d888e39af5f185
Keitumetse <kr.molefe98@gmail.com>
2024-05-03 09:40:24 +0200

    Changed uuid methods

--- models.py
+++ models.py
@@ -1,42 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
 import uuid
-from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
-# Define database models
 class Author(db.Model):
-    id = db.Column(db.String(36), primary_key=True, default=uuid.uuid4)
+    """Model representing an author."""
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     name = db.Column(db.String(100), nullable=False)
-    birth_date = db.Column(db.String(20))
-    nationality = db.Column(db.String(50))
-
-    @staticmethod
-    def generate_id():
-        return str(uuid.uuid4())
 
 class Genre(db.Model):
-    id = db.Column(db.String(36), primary_key=True, default=uuid.uuid4)
+    """Model representing a genre."""
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text)
-
-    @staticmethod
-    def generate_id():
-        return str(uuid.uuid4())
+    description = db.Column(db.Text, nullable=True)
 
 class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    """Model representing a book."""
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     title = db.Column(db.String(200), nullable=False)
-    publication_date = db.Column(db.String(20))
+    publication_date = db.Column(db.String(20), nullable=True)
     isbn = db.Column(db.String(20))
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
-    genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'))
+    author_id = db.Column(db.String(36), db.ForeignKey('author.id'))
+    genre_id = db.Column(db.String(36), db.ForeignKey('genre.id'))
     cover_image = db.Column(db.String(255))
 
     author = db.relationship('Author', backref='books')
     genre = db.relationship('Genre', backref='books')
-
-    @staticmethod
-    def generate_id():
-        return str(uuid.uuid4())
