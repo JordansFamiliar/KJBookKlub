afaf0859d621d0e2c4a58d575ed6b34baa10cf73
Keitumetse <kr.molefe98@gmail.com>
2024-05-06 22:39:07 +0200

    Added test to retrieve book by ID

--- test_api_endpoints.py
+++ test_api_endpoints.py
@@ -13,3 +13,9 @@ class TestAPIEndpoints(unittest.TestCase):
     def test_get_all_books(self):
         response = self.app.get('/mysite/books')
         self.assertEqual(response.status_code, 200)
+
+    def test_get_book_by_id(self):
+        response = self.app.get('/mysite/books/1')
+        self.assertEqual(response.status_code, 200)
+
+
